8bc3ff90621ebd42617c78ef8ba5e5632eecbb30
anatel <anat.eliahu@gmail.com>
2019-07-27 22:09:52 +0300

    Add styled-components and global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Home from './pages/Home';
+import GlobalStyles from './components/globals/globalStyles';
 
 function App() {
     return (
         <>
+            <GlobalStyles/>
             <Home/>
         </>
     );
